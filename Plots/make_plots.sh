16b638c491eb6760fc742c40d2b01b86226bb091
Ian Reed <ireed3597@protonmail.com>
2023-10-09 06:34:14 -0700

    Updates towards S+B band plots

--- Plots/make_plots.sh
+++ Plots/make_plots.sh
@@ -2,33 +2,33 @@
 
 set -x
 
-source /cvmfs/cms.cern.ch/cmsset_default.sh
-source /vols/grid/cms/setup.sh
+#source /cvmfs/cms.cern.ch/cmsset_default.sh
+#source /vols/grid/cms/setup.sh
 
 tag=SM_22Sep23_fixed_dijet_dummies
 trees=/home/users/iareed/CMSSW_10_2_13/src/flashggFinalFit/files_systs/$tag/
 
-cmsenv
-source setup.sh
+#cmsenv
+#source setup.sh
 
-nToys=500
+nToys=10
 
 make_toys(){
     pushd Plots 
         rm -rf SplusBModels$tag
-        python makeToys.py --inputWSFile ../Combine/Datacard_ttHHggXX.root --ext $tag --dryRun --nToys $nToys
+        python makeToysHH.py --inputWSFile /home/users/iareed/CMSSW_10_2_13/src/flashggFinalFit/Combine/Datacard_ttHHggXX.root --ext $tag --dryRun --nToys $nToys --dropResonantBkg
         iter=0
         while [ $iter -lt $nToys ]
         do                      
-            ./SplusBModels${tag}/toys/jobs/sub_toy_$(($iter+1)).sh      &
-            ./SplusBModels${tag}/toys/jobs/sub_toy_$(($iter+2)).sh      &
-            ./SplusBModels${tag}/toys/jobs/sub_toy_$(($iter+3)).sh      &
-            ./SplusBModels${tag}/toys/jobs/sub_toy_$(($iter+4)).sh      &
-            ./SplusBModels${tag}/toys/jobs/sub_toy_$(($iter+5)).sh      &
-            ./SplusBModels${tag}/toys/jobs/sub_toy_$(($iter+6)).sh      &
-            ./SplusBModels${tag}/toys/jobs/sub_toy_$(($iter+7)).sh      &
-            ./SplusBModels${tag}/toys/jobs/sub_toy_$(($iter+8)).sh      &
-            ./SplusBModels${tag}/toys/jobs/sub_toy_$(($iter+9)).sh      &
+            ./SplusBModels${tag}/toys/jobs/sub_toy_$(($iter+1)).sh &
+            ./SplusBModels${tag}/toys/jobs/sub_toy_$(($iter+2)).sh &
+            ./SplusBModels${tag}/toys/jobs/sub_toy_$(($iter+3)).sh &
+            ./SplusBModels${tag}/toys/jobs/sub_toy_$(($iter+4)).sh &
+            ./SplusBModels${tag}/toys/jobs/sub_toy_$(($iter+5)).sh &
+            ./SplusBModels${tag}/toys/jobs/sub_toy_$(($iter+6)).sh &
+            ./SplusBModels${tag}/toys/jobs/sub_toy_$(($iter+7)).sh &
+            ./SplusBModels${tag}/toys/jobs/sub_toy_$(($iter+8)).sh &
+            ./SplusBModels${tag}/toys/jobs/sub_toy_$(($iter+9)).sh &
             ./SplusBModels${tag}/toys/jobs/sub_toy_$iter.sh                                     
             iter=$(($iter+10))
         done
@@ -37,13 +37,13 @@ make_toys(){
 
 make_SpB(){
     pushd Plots 
-        python makeSplusBModelPlot.py --inputWSFile ../Combine/Datacard_ttHHggXX.root --cat "SR1" --doBands --ext $tag --parameterMap r:0 #--parameterMap ".*/ttHHggbb.*:r[1,0,2]"
-        #python makeSplusBModelPlot.py --inputWSFile ../Combine/Datacard_ttHHggXX.root --cat "SR2" --doBands --ext $tag #--parameterMap ".*/ttHHggbb.*:r[1,0,2]"
+        python makeSplusBModelPlot.py --inputWSFile /home/users/iareed/CMSSW_10_2_13/src/flashggFinalFit/Combine/Datacard_ttHHggXX.root --cat "SR1" --doBands --ext $tag #--parameterMap r:0 #--parameterMap ".*/ttHHggbb.*:r[1,0,2]"
+#        python makeSplusBModelPlot.py --inputWSFile /home/users/iareed/CMSSW_10_2_13/src/flashggFinalFit/Combine/Datacard_ttHHggXX.root --cat "SR2" --doBands --ext $tag #--parameterMap r:0 #--parameterMap ".*/ttHHggbb.*:r[1,0,2]"
 
         cp SplusBModels$tag/*.png /home/users/iareed/public_html/ttHH/flashggFinalFit/$tag/Bands/
     popd        
 
 }
 
-#make_toys
+make_toys
 make_SpB
