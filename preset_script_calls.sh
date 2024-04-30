65c3c170e43b9c89d48283be52f6c095ffa699c7
Ian Reed <ireed3597@protonmail.com>
2024-04-30 03:07:42 -0700

    Insctructions for Giacomo

---
+++ preset_script_calls.sh
@@ -0,0 +1,9 @@
+#! /usr/bin/bash
+
+#Update the tag in XXXX_get_limit_systematics.sh file to the one passed to the converter in HggNanoanalys
+#Toggle the commands at the bottom of the file to chose which part runs
+#After making the datacard_XXXX_tag.txt file, check for 0.000/2.000 or 2.000/0.000 and turn them to 0.001/1.999 or 1.999/0.001 as needed
+#Should only be an issue with a few passes of Tprime
+
+#The S+B plots requrie a bit more complicated handling that I haven't automated yet
+#Just leave them for me
